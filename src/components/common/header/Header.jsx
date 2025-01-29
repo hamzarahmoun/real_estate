285319add4131abb53e806642608294dfc06be7b
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-29 12:47:20 +0100

    navbar links

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import "./header.css"
-
+import { nav } from "../../data/Data"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   const [navList, setNavList] = useState(false)
@@ -12,7 +13,15 @@ const Header = () => {
           <div className='logo'>
             <img src='./images/logo.png' alt='' />
           </div>
-        
+          <div className='nav'>
+            <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className='button flex'>
             <h4>
               <span>2</span> My List
