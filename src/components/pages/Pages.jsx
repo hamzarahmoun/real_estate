d3cdf202122d44230d250a903f66f4d5e73453e3
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-31 10:54:05 +0100

    add hero strcuure

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Header from "../common/header/Header"
-import { BrowserRouter as Router } from "react-router-dom"
+import { Route, BrowserRouter as Router } from "react-router-dom"
 import Footer from "../common/footer/Footer"
 
 
@@ -10,8 +10,10 @@ function  Pages  () {
     <>
       <Router>
         <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
         <Footer />
-        
       </Router>
     </>
   )
