ee0a853d31cc47793c939c826dbd8cb90e5ba77e
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-02-02 21:08:22 +0100

    featured strcture

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Hero from "./hero/Hero"
+import Featured from "./featured/Featured"
 
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Featured />
       
     </>
   )
