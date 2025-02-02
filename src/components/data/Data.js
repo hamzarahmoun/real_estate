87d76df5644a572bd9b467319f2091c04cfc53c0
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-02-02 21:13:23 +0100

    featured card

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -38,3 +38,31 @@ export const nav = [
       text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
     },
   ]
+
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "Family House",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "House & Villa",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "Apartment",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "Office & Studio",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: "Villa & Condo",
+      total: "80 Property",
+    },
+  ]
--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import Heading from "../../common/Heading"
 import "./Featured.css"
-// import FeaturedCard from "./FeaturedCard"
+import FeaturedCard from "./FeaturedCard"
 
-const Featured = () => {
+function  Featured ()  {
   return (
     <>
       <section className='featured background'>
         <div className='container'>
           <Heading title='Featured Property Types' subtitle='Find All Type of Property.' />
-          {/* <FeaturedCard /> */}
+          <FeaturedCard />
         </div>
       </section>
     </>
