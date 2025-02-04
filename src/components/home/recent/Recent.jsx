317189d38c78c37842d8fe1c6c5e0fcf74b044e8
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-02-04 18:33:43 +0100

    recent card

--- src/components/home/recent/Recent.jsx
+++ src/components/home/recent/Recent.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Heading from "../../common/Heading"
 import "./recent.css"
+import RecentCard from "./RecentCard"
 
 const Recent = () => {
   return (
@@ -8,6 +9,7 @@ const Recent = () => {
       <section className='recent padding'>
         <div className='container'>
           <Heading title='Recent Property Listed' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
+          <RecentCard />
         </div>
       </section>
     </>
