21899fa604b6f019ab71f36fcce4047668c653bf
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-02-04 18:28:12 +0100

    recent section structure

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Hero from "./hero/Hero"
 import Featured from "./featured/Featured"
+import Recent from "./recent/Recent"
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <>
       <Hero />
       <Featured />
+      <Recent />
       
     </>
   )
