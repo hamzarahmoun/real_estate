18eaabee6f6d1aee8e3a9db6cc589f71d9d8526e
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-02-06 22:08:09 +0100

    location strcuture

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "./hero/Hero"
 import Featured from "./featured/Featured"
 import Recent from "./recent/Recent"
 import Awards from "./awards/Awards"
+import Location from "./location/Location"
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <Featured />
       <Recent />
       <Awards />
+      <Location />
       
     </>
   )
